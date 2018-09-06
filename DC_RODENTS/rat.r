bdbfb564855b686494b4e62b076758156b27c14d
John D. Harden <Jdharden123@gmail.com>
2018-09-06 15:35:39 -0400

    Add files via upload

---
+++ DC_RODENTS/rat.r
@@ -0,0 +1,14 @@
+library (tidyverse)
+library (ggplot2)
+library (tidyr)
+library (dplyr)
+library (readr) 
+
+dc_rats <- read.csv ("2018_311 Rodent Calls.csv", stringsAsFactors=FALSE, na.strings="#VALUE!", header = TRUE)
+
+head(dc_rats)
+summary(dc_rats)
+str(dc_rats)
+
+ggplot ( data = dc_rats ) + 
+geom_point ( mapping = aes ( x = LATITUDE , y = LONGITUDE, color = WARD))
