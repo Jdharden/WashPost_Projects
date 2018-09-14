ba1316dcbeff7e2bb1040ee0030c938cbb367b9b
John D. Harden <Jdharden123@gmail.com>
2018-09-14 15:30:40 -0400

    Update rat.r

--- DC_RODENTS/rat.r
+++ DC_RODENTS/rat.r
@@ -3,9 +3,16 @@ library (ggplot2)
 library (tidyr)
 library (dplyr)
 library (readr) 
+library(raster)
+
+
 
 dc_rats <- read.csv ("2018_311 Rodent Calls.csv", stringsAsFactors=FALSE, na.strings="#VALUE!", header = TRUE)
+dc<-subset(us,NAME_1=="District of Columbia", level=5)
+unique(us$NAME_1)
 
+
+plot(dc)
 head(dc_rats)
 summary(dc_rats)
 str(dc_rats)
